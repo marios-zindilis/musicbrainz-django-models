d91b5e2d0d345009beb16b7727c8cbd545d42e04
Marios Zindilis <marios@zindilis.com>
2017-04-29 18:06:59 +0100

    more tests

--- musicbrainz_django_models/tests/test_area.py
+++ musicbrainz_django_models/tests/test_area.py
@@ -3,29 +3,33 @@ from ..models import area_type, area
 
 
 class test_area(TestCase):
-    def test_str(self):
+    def test__area__instance(self):
+        subject = area(name='Heaven')
+        self.assertIsInstance(subject, area)
+
+    def test__area__str(self):
         subject = area(name='Heaven')
         self.assertEqual(str(subject), subject.name)
 
-    def test_unicode(self):
+    def test__area__unicode(self):
         subject = area(name='Hell')
         self.assertEqual(subject.__unicode__(), subject.name)
 
-    def test_end_date_year(self):
+    def test__area__end_date_year(self):
         at = area_type(name='Country')
         at.save()
         subject = area(name='Utopia', end_date_year=2012, type=at)
         subject.save()
         self.assertTrue(subject.ended)
 
-    def test_end_date_month(self):
+    def test__area__end_date_month(self):
         at = area_type(name='Country')
         at.save()
         subject = area(name='Fruitopia', end_date_month=12, type=at)
         subject.save()
         self.assertTrue(subject.ended)
 
-    def test_end_date_day(self):
+    def test__area__end_date_day(self):
         at = area_type(name='Country')
         at.save()
         subject = area(name='Footopia', end_date_day=12, type=at)
